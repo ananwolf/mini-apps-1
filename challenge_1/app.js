f11dcf2461439ec11e8758f3a397fc2e511ac535
Anan Wolf <ananwolf1@gmail.com>
2021-05-24 14:53:03 -0700

    skeleton out the code for app

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,39 @@
-// set up a board state of array
-let 
-// create a game counter
+// set up a board state of array 8 indexes
+// let the game board be empty arays so we can fill in X O
 
-//
+/* GLOBAL VARIABLES */
+// Game Board
+let gameState = ['', '', '', '', '', '', '', '', ''];
+// first player X
+let currentPlayer = 'X'
+// game counter
+let gamesPlayed = 0;
 
+/* FUNCTIONS */
+// check if box is filled
+handlePlayedBox = () => {
+
+}
+// switching between X & O players
+handlePlayerChange = () => {
+
+}
+// handle click event for each box
+handleboxClick = () => {
+
+}
+// check if anyone won
+handleWinner = () => {
+
+}
+// clear current board
+handleClearButton = () => {
+
+}
+// reset entire game
+handleResetButton = () => {
+
+}
+
+// TODO
+// add click functionality to buttons, boxes
